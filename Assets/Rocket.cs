2f5a6d5951820f5a6992613b33191defc3abefc1
Mazen Morgan <morgan.mazen42@gmail.com>
2017-11-29 12:30:18 +0200

    Physics and Rigibodies

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-    
+
+    Rigidbody rigidbody;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -21,7 +23,7 @@ public class Rocket : MonoBehaviour {
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            print("Thrusting");
+            rigidbody.AddRelativeForce(Vector3.up);
         }
         if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
