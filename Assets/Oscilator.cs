9e7f49306915e4f389b1107315504ec1b59d1203
Mazen Morgan <morgan.mazen42@gmail.com>
2017-12-01 13:14:28 +0200

    Moving Platform pattern

---
+++ Assets/Oscilator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscilator : MonoBehaviour {
+    [SerializeField] Vector3 movementVector;
+    [Range(0, 1)] [SerializeField] float movementFactor;
+    Vector3 startingPos;
+	// Use this for initialization
+	void Start ()
+    {
+        startingPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = offset + startingPos;
+		
+	}
+}
